213d0ec149df10858d0b241550a4d35439b1e396
Paulo Filho <pcf.feat@gmail.com>
2022-09-16 14:10:30 -0400

    get image from the internet and display it in the imageView

--- RandomPhoto/ViewController.swift
+++ RandomPhoto/ViewController.swift
@@ -22,8 +22,19 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
         imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         imageView.center = view.center
+        
+        getRandomPhoto()
     }
 
 
+    func getRandomPhoto() {
+        let urlString = "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        
+        imageView.image = UIImage(data: data)
+    }
 }
 
